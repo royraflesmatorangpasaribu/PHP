7d6762a700e57e6387f36f4ead4b5dfa66606781
Roy Rafles Matorang Pasaribu <90742736+royraflesmatorangpasaribu@users.noreply.github.com>
2023-08-05 21:30:27 +0700

    Create GameTebakAngka.php

---
+++ GameTebakAngka.php
@@ -0,0 +1,24 @@
+// Dibuat Oleh Roy Rafles Matorang Pasaribu 
+// pada 05 Agustus 2023
+
+<?php
+while(true){
+    
+echo "Welcome To My Game\n"; 
+echo "===== Game Tebak Angka =====\n";
+echo "Tebak sebuah angka antara 1 dan 9!\n";
+$computer = rand(1,9);
+echo "Masukkan Tebakanmu : ";
+$player = trim(fgets(STDIN));
+echo "Result => ";
+if($player == $computer){
+    echo "MENANG\n";
+}
+else{
+    echo "KALAH\n";
+}
+echo "Angka Yang diberikan komputer : $computer\n";
+echo "Angka Yang kamu Tebak : $player\n\n\n";
+    
+}
+
